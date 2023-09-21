7148405a6f534c3388655a7783d6447162d29fbe
Lord Zurra <etana@etanaisen.net>
2023-09-21 22:57:24 +0300

    Project to BP only,

--- Source/LXRDev/LXRDev.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "LXRDev.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LXRDev, "LXRDev" );
